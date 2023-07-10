93e6e04361fdf743c3a145f9e73db676ed479bb7
jayimf432 <jayimf432@gmail.com>
2023-07-10 14:09:51 -0400

    added hello world

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Halo Billing System")
 window.configure(background="Grey")
-window.geometry("640x480")
+window.geometry("1440x1000")
+message = tk.Label(window, text="Hello, World!")
+message.pack()
 window.mainloop()
