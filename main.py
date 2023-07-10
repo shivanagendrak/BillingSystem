d3538b75f80f20da9f5f9a55c231c61d368816fe
jayimf432 <jayimf432@gmail.com>
2023-07-10 14:00:59 -0400

    changed the resolution

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import tkinter as tk
 window = tk.Tk()
 window.title("Halo Billing System")
 window.configure(background="Grey")
-window.geometry("1280x720")
+window.geometry("640x480")
 window.mainloop()
